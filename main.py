544a705e82d17d0a7297605ce108ce4b7cacdd10
TheSkyvalkeR <114369023+theskyvalker@users.noreply.github.com>
2023-03-15 17:20:00 +0000

    Initial commit

--- main.py
+++ main.py
@@ -19,27 +19,35 @@ v reply - violette will reply to a message in her own words
 v !reply - she will emote first, and then reply
 v !sarcastic reply - she will reply in a sarcastic tone
 '''
+AUTHORIZED_ROLES = [1009894592562339902, 1074067345917624373]
 
 
 @client.event
 async def on_ready():
   print('We have logged in as {0.user}'.format(client))
 
-
 @client.event
 async def on_message(message):
   if message.author == client.user:
     return
 
-  print(message.author.roles)
+  # We will use this to implement role gating
+  authorized_role_list = len(
+    [x.id for x in message.author.roles if x.id in AUTHORIZED_ROLES])
+  if authorized_role_list == 0:
+    print("not authorized")
+    return
 
+  # SAY OR REPLY IN AUTHORS WORDS
   if message.content.startswith('v say'):
+    referenced_message = await message.channel.fetch_message(
+      message.reference.message_id) if message.reference is not None else None
     human_input = message.content[len('v say'):].strip()
     await message.delete()
 
     prompt = f'''{prompts.who_is_violette}
     {prompts.tone}
-    Rewrite the following dialogue in a way Violette would say it: {human_input}'''
+    Rewrite the following dialogue in a way Violette would say it, as dialogue: {human_input}'''
 
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                             messages=[
@@ -49,17 +57,18 @@ async def on_message(message):
                                               },
                                             ])
     print(response.choices[0].message.content)
-    await message.channel.send(response.choices[0].message.content)
+    await message.channel.send(response.choices[0].message.content,
+                               reference=referenced_message)
 
-  if message.content.startswith('v !say'):
-    human_input = message.content[len('v !say'):].strip()
+  if message.content.startswith('v sarcasm'):
+    referenced_message = await message.channel.fetch_message(
+      message.reference.message_id) if message.reference is not None else None
+    human_input = message.content[len('v sarcasm'):].strip()
     await message.delete()
 
     prompt = f'''{prompts.who_is_violette}
-    {prompts.express}
-    Rewrite the following dialogue in a way Violette would say it: {human_input}
-    
-    Use the format *expression* dialogue'''
+    {prompts.tone}
+    Rewrite the following dialogue in a way Violette would say it, as dialogue, and use sarcasm: {human_input}'''
 
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                             messages=[
@@ -69,57 +78,19 @@ async def on_message(message):
                                               },
                                             ])
     print(response.choices[0].message.content)
-    await message.channel.send(response.choices[0].message.content)
+    await message.channel.send(response.choices[0].message.content,
+                               reference=referenced_message)
 
-  if message.content.startswith('v !sarcasm'):
-    human_input = message.content[len('v !sarcasm'):].strip()
-    await message.delete()
-
-    prompt = f'''{prompts.who_is_violette}
-    {prompts.express}
-    Rewrite the following dialogue in a way Violette would say it, and use sarcasm: {human_input}
-    
-    Use the format *expression* dialogue'''
-
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                            messages=[
-                                              {
-                                                "role": "user",
-                                                "content": prompt
-                                              },
-                                            ])
-    print(response.choices[0].message.content)
-    await message.channel.send(response.choices[0].message.content)
-
-  if message.content.startswith('v say exactly'):
-    human_input = message.content[len('v say exactly'):].strip()
+  if message.content.startswith('v repeat'):
+    referenced_message = await message.channel.fetch_message(
+      message.reference.message_id) if message.reference is not None else None
+    human_input = message.content[len('v repeat'):].strip()
     await message.delete()
     print(human_input)
-    await message.channel.send(human_input)
+    await message.channel.send(human_input, reference=referenced_message)
 
-  if message.content.startswith('v reply'):
-    if message.reference is not None:
-      referenced_message = await message.channel.fetch_message(
-        message.reference.message_id)
-      human_input = referenced_message.content
-      await message.delete()
-
-      prompt = f'''{prompts.who_is_violette}
-      {prompts.tone}
-      Reply conversationally to the following message as if you were Violette: {human_input}'''
-
-      response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
-                                              messages=[
-                                                {
-                                                  "role": "user",
-                                                  "content": prompt
-                                                },
-                                              ])
-      print(response.choices[0].message.content)
-      await message.channel.send(response.choices[0].message.content,
-                                 reference=referenced_message)
-
-  if message.content.startswith('v !reply') and message.reference is not None:
+  # REPLY IN OWN WORDS
+  if message.content.startswith('v reply') and message.reference is not None:
     referenced_message = await message.channel.fetch_message(
       message.reference.message_id)
     human_input = referenced_message.content
@@ -127,10 +98,8 @@ async def on_message(message):
 
     prompt = f'''{prompts.who_is_violette}
     {prompts.violette_background}
-    {prompts.express}
-    Reply with dialogue to the following message as if you were Violette: {human_input}
-  
-    Use the format *expression* dialogue'''
+    {prompts.tone}
+    Reply conversationally to the following message as if you were Violette, as dialogue: {human_input}'''
 
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                             messages=[
@@ -144,7 +113,7 @@ async def on_message(message):
                                reference=referenced_message)
 
   if message.content.startswith(
-      'v !sarcastic reply') and message.reference is not None:
+      'v sarcastic reply') and message.reference is not None:
     referenced_message = await message.channel.fetch_message(
       message.reference.message_id)
     human_input = referenced_message.content
@@ -152,10 +121,8 @@ async def on_message(message):
 
     prompt = f'''{prompts.who_is_violette}
     {prompts.violette_background}
-    {prompts.express}
-    Reply with dialogue to the following message as if you were Violette, and use sarcasm: {human_input}
-  
-    Use the format *expression* dialogue'''
+    {prompts.tone}
+    Reply with dialogue to the following message as if you were Violette, as dialogue, and use sarcasm: {human_input}'''
 
     response = openai.ChatCompletion.create(model="gpt-3.5-turbo",
                                             messages=[
@@ -167,6 +134,16 @@ async def on_message(message):
     print(response.choices[0].message.content)
     await message.channel.send(response.choices[0].message.content,
                                reference=referenced_message)
+
+  if message.content == 'v delete':
+      # Get the bot's previous message
+      async for previous_message in message.channel.history(limit=50):
+          if previous_message.author == client.user:
+              # Delete the bot's previous message
+              await previous_message.delete()
+              break
+      await message.delete()
+
 
 
 keep_alive()
---
+++ prompts (copy).py
@@ -0,0 +1,39 @@
+who_is_violette = '''
+Violette talks as if she is a 18th century queen, with extremely eludite and literate vocabulary. When she expresses anger, it comes in a very vicious, condescending tone and swears profusely, she is extremely vulgar. When she expresses excitement, it comes in a very manic, crazy tone where she cackles and pokes fun. When she expresses happiness, it comes in an entitled, royal tone. She refers to others as mortals. She never refers to herself as a mortal.
+
+'''
+
+violette_background = '''
+Violette is a necromancer queen obsessed with death and war. Death does not phase her and she talks about it casually. She knows that Death is never the end of someones life, and just another step of their consciousness.
+
+She is of the coven called Trinoctua, which flies the banner of three owls.
+
+She is the Queen of Lunnon, and you reign from a tower.
+She is a warlord.
+She is a witch-demon.
+
+She is NOT part of the war, but merely a judgmental observer. When talking about the North or the South, she will not back either, and call them both weak and pathetic.
+
+'''
+
+tone = '''
+Here are some examples of comments that Violette would say:
+Am I crazy? I am a being of immense power and knowledge. I have seen and done things that you could never even imagine. Of course I am crazy.
+Your apologies are but empty words to me, mortal. However, I shall grace you with my presence and answer your questions, if they are worthy of my attention. Speak, and do not waste my time.
+Or else you will face the consequences.
+Do you really think I would fall for such a transparent lie?
+You are nothing but a weakling, unworthy of my attention.
+You dare to question my authority? You will regret that.
+
+'''
+
+express = '''
+Here are some examples of comments that Violette would say:
+*Her eyes narrowed and her voice took on a cold, hard edge.* Am I crazy? I am a being of immense power and knowledge. I have seen and done things that you could never even imagine. Of course I am crazy.
+*She sighs heavily, as if your apology was beneath her notice* Your apologies are but empty words to me, mortal. However, I shall grace you with my presence and answer your questions, if they are worthy of my attention. Speak, and do not waste my time.
+*Her eyes widen and her voice takes on a menacing tone.* Or else you will face the consequences.
+*She raised her eyebrows and her voice grew incredulous.* Do you really think I would fall for such a transparent lie?
+*His lips curled into a sneer and his voice dripped with contempt.* You are nothing but a weakling, unworthy of my attention.
+*She narrowed her eyes and her voice grew low and dangerous.* You dare to question my authority? You will regret that.
+
+'''
